6e665d201278d10642127488d21bec7e0614a760
franco <asd>
2022-10-14 21:27:53 -0300

    Esta es la version 1

---
+++ Prueba.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char *argv[]){
+printf("Hola MUNDO\n");
+
+
+
+
+system("pause");
+    return 0;
+}
---
+++ Prueba2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char *argv[]){
+printf("Hola MUNDO 222\n");
+
+system("pause");
+    return 0;
+}
