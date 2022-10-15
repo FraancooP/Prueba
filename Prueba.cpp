c74cd871ba4d9fb1e78bc52d28e13a28c2203337
franco <asd>
2022-10-14 21:31:16 -0300

    Esta es la version 2

--- Prueba.cpp
+++ Prueba.cpp
@@ -2,9 +2,7 @@
 using namespace std;
 int main(int argc, char *argv[]){
 printf("Hola MUNDO\n");
-
-
-
+printf("Como estas\n");
 
 system("pause");
     return 0;
--- Prueba2.cpp
+++ Prueba2.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(int argc, char *argv[]){
 printf("Hola MUNDO 222\n");
-
+printf("ASDASDA 212\n");
 system("pause");
     return 0;
 }
