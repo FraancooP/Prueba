0218dc2a366ce4b25966229ff2f067b46b7b0646
agusclap <rodeyroagustin@gmail.com>
2022-10-21 20:22:27 -0300

    Agregue Ejecicio

---
+++ Carpeta_nueva/Ejercicio.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+
+    cout<<"Numeros del 1 al 10"<<endl;
+    for(int i=1;i<11;i++){
+
+        cout<<i<<endl;
+    }
+    
+    cout<<"Programa finalizado con exito ;)"<<endl;
+
+    return 0;
+}
